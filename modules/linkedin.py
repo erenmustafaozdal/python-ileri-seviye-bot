33f580bc4b2f26d91c624ccdc5426dfac6f5b3c0
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-04-08 10:13:16 +0300

    Linkedin post bilgilerini alma

--- modules/linkedin.py
+++ modules/linkedin.py
@@ -5,13 +5,11 @@ from selenium.webdriver.common.by import By
 class LinkedIn:
 
     # SEÇİCİLER
-    # post = //div[contains(@class,'occludable-update')]
-    # post_owner_link = //a[contains(@class, 'update-components-actor__container-link')]
-    # post_owner = //span[contains(@class, 'update-components-actor__name')]
-    # post_date = //span[contains(@class, 'update-components-actor__sub-description')]
-    # post_text = //div[contains(@class, 'feed-shared-update-v2__description-wrapper')]
-    company_name = "//h1[contains(@class, 'ember-view')]"
-    post_title = "//div[contains(@class, 'update-components-actor__meta')]"
+    post = "//div[contains(@class,'occludable-update')]"
+    post_owner_link = ".//a[contains(@class, 'update-components-actor__container-link')]"
+    post_owner = ".//span[contains(@class, 'update-components-actor__name')]"
+    post_date = ".//span[contains(@class, 'update-components-actor__sub-description')]"
+    post_text = ".//div[contains(@class, 'feed-shared-update-v2__description-wrapper')]//div[contains(@class, 'update-components-text')]"
 
     def __init__(self, driver: Chrome):
         self.driver = driver
@@ -37,15 +35,28 @@ class LinkedIn:
         # Şirketin profiline git
         self.driver.get(f"https://www.linkedin.com/company/{company}/posts")
 
-        # Şirketin adını al
-        company_name = self.driver.find_element(By.XPATH, self.company_name).text
-
         # gönderileri al
-        post_titles = self.driver.find_elements(By.XPATH, self.post_title)
+        posts = self.driver.find_elements(By.XPATH, self.post)
 
         # Gönderiler içinde döngüye girelim
-        for title in post_titles:
-            pass
-            # todo: Gönderinin tarihi seçicisi
-            # todo: Gönderinin metni seçicisi
+        result = []
+        for post in posts:
+            # Gönderileri reklamlardan ayırmak için gönderi sahibi linki
+            # var mı diye kontrol edelim. Eğer yoksa continue ile adımı atlayalım
+            try:
+                post_link = post.find_element(By.XPATH, self.post_owner_link).get_attribute("href")
+                post_owner_name = post.find_element(By.XPATH, self.post_owner).text
+                post_date = post.find_element(By.XPATH, self.post_date).text
+                post_text = post.find_element(By.XPATH, self.post_text).text
+            except:
+                continue
 
+            # todo: dict key'ler düzeltilecek dict.values ile excel'e yazılacak
+            result.append({
+                "A": post_owner_name.split("\n")[0],
+                "B": post_link.split("?")[0],
+                "C": post_date.split(' • ')[0],
+                "D": post_text
+            })
+
+        return result
